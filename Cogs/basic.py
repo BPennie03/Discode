69e70a70e467587b2706925c059d542da69bcb6f
Braden Pennie <pennie.3@wright.edu>
2023-10-19 16:42:17 -0400

    Added sync command to sync slash commands

--- Cogs/basic.py
+++ Cogs/basic.py
@@ -9,6 +9,12 @@ async def setup(bot:commands.Bot):
 class StudentCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        
+    @commands.command(name='sync')
+    async def sync(self, ctx):
+        await self.bot.tree.sync()
+        self.bot.tree.copy_global_to(guild=ctx.guild)
+        await ctx.send(f'All slash commands have been synced')
 
     @app_commands.command(description="Sends message containing Discord WebSocket protocol latency")
     async def ping(self, interaction:discord.Interaction):
