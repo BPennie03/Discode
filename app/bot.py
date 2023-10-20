806dc7ba7f5b3d3ceb5eab3313dbaf351d5d6d69
Reese Hatfield <hatfield.69@wright.edu>
2023-10-20 14:42:01 -0400

    added docker

--- app/bot.py
+++ app/bot.py
@@ -16,7 +16,7 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
 
-    for file in os.listdir('Cogs'):
+    for file in os.listdir('app'):
         if not file.startswith('__') and file.endswith('.py'):
             try:
                 await bot.load_extension(f'Cogs.{file[:-3]}')
--- app/dockertest.py
+++ app/dockertest.py
@@ -1,7 +1,11 @@
+import discord
+# import from requirements
+
 
 def main():
     print("Ran from Docker!")
 
 
+# change the docker file to run this to test if docker is working
 if __name__ == "__main__":
     main()
