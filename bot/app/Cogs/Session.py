cba9a1fe2cb4bd9b6eba52917026fa6f694553b9
Braden Pennie <pennie.3@wright.edu>
2024-04-02 14:25:35 -0400

    Created basic cog for sessions

---
+++ bot/app/Cogs/Session.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Session(bot))
+
+
+class Session(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
