7cedcfb949a6c2ec5f66f1024dfc7cb2aaa0cf10
ReeseHatfield <hatfield.69@wright.edu>
2024-04-03 13:02:20 -0400

    add check_session command
    also converted things to slash commands

--- bot/app/Cogs/Session.py
+++ bot/app/Cogs/Session.py
@@ -10,30 +10,57 @@ async def setup(bot: commands.Bot):
 class Session(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.active_users = {}
+        self.active_users = set()
 
     # Command to begin/start a session
-    @commands.command(name='start')
-    async def start(self, ctx):
-        id = self.get_id_from_ctx(ctx)
-        
+    @app_commands.command(description='Start Discode Session')
+    async def start(self, interaction: discord.Interaction):
+        id = self.get_id_from_xaction(interaction)
+        name = interaction.user.global_name
+
+        if id in self.active_users:
+            await interaction.response.send_message(
+                f"{name} already has an active session",
+                ephemeral=True
+            )
+            return
+
         self.active_users.add(id)
-        await ctx.send(f"{ctx.author.name} (ID: {id}) has started a session")
-    
+        await interaction.response.send_message(f"{name} (ID: {id}) has started a session")
+
     # Command to end a session
-    @commands.command(name='stop')
-    async def stop(self, ctx):
-        id = self.get_id_from_ctx(ctx)
-        
+    @app_commands.command(description='Stop Discode Session')
+    async def stop(self, interaction: discord.Interaction):
+        id = self.get_id_from_xaction(interaction)
+        name = interaction.user.global_name
+
+        if id not in self.active_users:
+            await interaction.response.send_message(
+                f"{name} has no session to end", 
+                ephemeral=True
+            )
+
         try:
             self.active_users.remove(id)
         except KeyError:
-            await ctx.send(f"ID {id} not found in active users")
+            await interaction.response.send_message(
+                f"ID {id} not found in active users", 
+                ephemeral=True    
+            )
             return
+
+        await interaction.response.send_message(f"User {name} (ID: {id}) has ended their session")
         
-        await ctx.send(f"User {ctx.author.name} (ID: {id}) has ended their session")
+    @app_commands.command(description='Check to see if you have an active session')
+    async def check_session(self, interaction: discord.Interaction):
+        id = self.get_id_from_xaction(interaction)
+        status = id in self.active_users
         
-    
+        message = "Your session is currently active." if status else "You do not have an active session."
+        
+        await interaction.response.send_message(message, ephemeral=True)
+        
+
     @staticmethod
-    def get_id_from_ctx(ctx):
-        return ctx.author.name
+    def get_id_from_xaction(interaction: discord.Interaction):
+        return interaction.user
