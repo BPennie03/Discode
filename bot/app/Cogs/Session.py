b0052c6b1ec11df1c5a230d96f13c5b449a56604
ReeseHatfield <hatfield.69@wright.edu>
2024-04-04 16:39:06 -0400

    pep8 compliance

--- bot/app/Cogs/Session.py
+++ bot/app/Cogs/Session.py
@@ -23,7 +23,7 @@ class Session(commands.Cog):
     async def start(self, interaction: discord.Interaction):
         """
         Start a code session with Discode
-        
+
         Parameters:
         - interaction (discord.Interaction): Interaction object sent from user
         """
@@ -45,7 +45,7 @@ class Session(commands.Cog):
     async def end(self, interaction: discord.Interaction):
         """
         End a code session with Discode
-        
+
         Parameters:
         - interaction (discord.Interaction): Interaction object sent from user
         """
@@ -54,7 +54,7 @@ class Session(commands.Cog):
 
         if id not in self.active_users:
             await interaction.response.send_message(
-                f"{name} has no session to end", 
+                f"{name} has no session to end",
                 ephemeral=True
             )
             return
@@ -63,37 +63,36 @@ class Session(commands.Cog):
             self.active_users.remove(id)
         except KeyError:
             await interaction.response.send_message(
-                f"ID {id} not found in active users", 
-                ephemeral=True    
+                f"ID {id} not found in active users",
+                ephemeral=True
             )
             return
 
         await interaction.response.send_message(f"User {name} (ID: {id}) has ended their session")
-        
+
     @app_commands.command(description='Check to see if you have an active session')
     async def check_session(self, interaction: discord.Interaction):
         """
         Check the status of the user's current discode session.
         In the future, this could potentially return more information pertaining to the sessions
         i.e. files ran, current lang, etc.
-        
+
         Parameters:
         - interaction (discord.Interaction): Interaction object sent from user
         """
         id = self.get_id_from_xaction(interaction)
         status = id in self.active_users
-        
+
         message = "Your session is currently active." if status else "You do not have an active session."
-        
+
         await interaction.response.send_message(message, ephemeral=True)
-        
 
     @staticmethod
     def get_id_from_xaction(interaction: discord.Interaction) -> discord.User:
         """
         Retrieves the user ID from discord interaction
         ID is currently just the user object, this is subject to change
-        
+
         Parameters:
         - interaction (discord.Interaction): The interaction from which the user object is to be retrieved.
 
